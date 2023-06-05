72a2a8a91cd2e0c631c1249affa44c30ca939401
AnnaHort <annahort3d@gmail.com>
2023-06-05 15:08:22 +0300

    task - 1
    зміна виводу у консоль, для виводу числових значень, а не рядків

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,17 @@
 // 1
 const categoriesEls = document.querySelectorAll('#categories > li.item');
 const itemsLength = [...categoriesEls].length;
-console.log(`Number of categories:${itemsLength}`);
+
+console.log('Number of categories:' + itemsLength);
 
 // 2
 
 const itemEls = document.querySelectorAll('li.item > h2');
-itemEls.forEach(item => console.log(`Category: ${item.firstChild.nodeValue}
- Elements: ${item.nextElementSibling.children.length}`));
 
-
+itemEls.forEach(item =>{
+    const categoryEl = item.firstChild.nodeValue;
+    const elementsEl  = item.nextElementSibling.children.length;
+    
+    console.log('Category:' + categoryEl);
+    console.log('Elements:' + elementsEl);
+})
